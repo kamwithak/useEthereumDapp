9a623b9a5f26e36a5d302262b1b1371929347121
Kamran Choudhry <theblizzzzard@gmail.com>
2022-01-21 18:29:24 -0500

    NEW: bootstrap account balance page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEtherBalance, useEthers} from '@usedapp/core'
+import { formatEther } from '@ethersproject/units'
 import './App.css';
 
 function App() {
+  const { activateBrowserWallet, account } = useEthers()
+  const etherBalance = useEtherBalance(account)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={() => activateBrowserWallet()}>Connect</button>
+      </div>
+      {account && <p>Account: {account}</p>}
+      {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
     </div>
-  );
+  )
 }
 
 export default App;
