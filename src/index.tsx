04d26e4690c14ecb671c5cdb486c3a05dbaac8af
Kamran Choudhry <theblizzzzard@gmail.com>
2022-01-21 18:31:12 -0500

    EDIT: adding dapp provider config for mainnet eth

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Mainnet, DAppProvider, Config } from '@usedapp/core'
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+const config: Config = {
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]: '',
+  },
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
