7f155fd355fa7822433b7cdac90c8d9a3c6c9699
Kamran Choudhry <theblizzzzard@gmail.com>
2022-01-21 22:06:02 -0500

    NEW: ready USD balance

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { useEtherBalance, useEthers} from '@usedapp/core'
-import { formatEther } from '@ethersproject/units'
+import { useEtherBalance, useEthers, useTokenBalance} from '@usedapp/core'
+import { formatEther, formatUnits } from '@ethersproject/units'
 import './App.css';
 
+const USD_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+
 function App() {
-  const { activateBrowserWallet, account } = useEthers()
-  const etherBalance = useEtherBalance(account)
+
+  const { activateBrowserWallet, account } = useEthers();
+  const etherBalance = useEtherBalance(account);
+  const tokenBalance = useTokenBalance(USD_ADDRESS, account);
+
   return (
     <div>
       <div>
         <button onClick={() => activateBrowserWallet()}>Connect</button>
       </div>
       {account && <p>Account: {account}</p>}
-      {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
+      {etherBalance && <p>ETH: {formatEther(etherBalance)}</p>}
+      {tokenBalance && <p>Balance: {formatUnits(tokenBalance, 6)} USD</p>}
     </div>
   )
 }
