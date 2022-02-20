f67a85a7b7c0543eef6946031c090f5b2cd176b8
Kamran Choudhry <theblizzzzard@gmail.com>
2022-02-19 22:24:08 -0500

    EDIT: connect with MM pg

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // Chakra UI
-import { Button } from "@chakra-ui/react"
+import { Button, Switch, useColorMode, Text, Flex, Spacer } from "@chakra-ui/react"
 import { Container } from '@chakra-ui/react'
 import { Center, Heading } from '@chakra-ui/react'
 import { Box } from '@chakra-ui/react'
 
 // Components
 import { WalletDapp } from './components/WalletDapp';
+import { Home } from './components/Home';
 
 // useDapp
 import { useEthers } from '@usedapp/core'
@@ -14,19 +15,46 @@ import { useEthers } from '@usedapp/core'
 import './App.css';
 
 const App = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDarkMode = colorMode === 'dark';
   const { activateBrowserWallet, account } = useEthers();
   if (account) {
     return <WalletDapp account={account}/>;
   }
   return (
-      <Container>
+    <Container
+      maxW='container.xl'
+      border='4px'
+      borderRadius='20px'
+      borderColor='pink.200'
+      style={{
+        marginTop: '15%'
+      }}>
+      <Flex>
         <Box>
-          <Heading>Please connect with MetaMask</Heading>
+          <Text style={{'paddingTop': '10px'}}>{isDarkMode ? 'Dark' : 'Light'} Mode</Text>
+          <Switch
+            colorScheme='pink'
+            onChange={toggleColorMode}
+            isChecked={colorMode === "light" ? true : false}
+          />
         </Box>
-        <Center bg='black' h='100px' color='white'>
-          <Button colorScheme='blue' onClick={() => activateBrowserWallet()}>Connect</Button>
-        </Center>
-      </Container>
+      </Flex>
+      <Flex justify="center">
+        <Box>
+          <Heading size='3xl' style={{'fontStyle': 'italic'}}>cryptowallet.xyz</Heading>
+        </Box>
+      </Flex>
+      <Flex
+        justify='right'
+        style={{'paddingBottom': '15px'}}>
+        <Button
+          colorScheme='pink'
+          borderRadius='12px'
+          onClick={() => activateBrowserWallet()}
+        >Connect</Button>
+      </Flex>
+    </Container>
   )
 }
 
