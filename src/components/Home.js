32ee9e4cfd2ec635d49ac96d4a80ab7171d0fe30
Kamran Choudhry <theblizzzzard@gmail.com>
2022-02-19 22:24:33 -0500

    NEW: example pg of dark/light mode hook

---
+++ src/components/Home.js
@@ -0,0 +1,56 @@
+import {
+    Container,
+    Text,
+    useColorMode,
+    Button,
+    IconButton,
+    Switch,
+    Icon
+  } from "@chakra-ui/react";
+  
+  import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+  
+  export const Home = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+  
+    return (
+      <Container
+        my="40px"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text>Hello User</Text>
+        <Button my="20px" onClick={toggleColorMode}>
+          Turn {colorMode === "light" ? "Dark" : "Light"}
+        </Button>
+  
+        <Switch
+          onChange={toggleColorMode}
+          isChecked={colorMode === "light" ? true : false}
+        />
+  
+        {colorMode === "light" ? (
+          <IconButton
+            my="20px"
+            isRound
+            color="gray.800"
+            icon={<MoonIcon />}
+            variant="outline"
+            onClick={toggleColorMode}
+          />
+        ) : (
+          <IconButton
+            my="20px"
+            isRound
+            color="yellow.500"
+            icon={<SunIcon />}
+            variant="outline"
+            onClick={toggleColorMode}
+          />
+        )}
+      </Container>
+    );
+  }
+  
