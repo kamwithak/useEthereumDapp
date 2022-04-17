1303ed2b815248e35da708afe7b4341145d541fa
Kamran Choudhry <theblizzzzard@gmail.com>
2022-04-16 22:09:53 -0400

    sdfsadfs

--- src/components/WalletDapp.js
+++ src/components/WalletDapp.js
@@ -10,7 +10,6 @@ import {
   Td,
   TableCaption,
 } from '@chakra-ui/react';
-
 const USD_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 const DAI_ADDRESS = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
 
